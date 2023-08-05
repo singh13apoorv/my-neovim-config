58db7cfd1a62148a4f0aea2178ee10887bcf6a4c
Apoorv Singh <singh13.apoorv@gmail.com>
2023-08-06 00:36:19 +0530

    making nvim transparent.

--- lua/apoorvsconfig/core/colorscheme.lua
+++ lua/apoorvsconfig/core/colorscheme.lua
@@ -28,15 +28,5 @@ require("gruvbox").setup({
   },
   overrides = {},
   dim_inactive = false,
-  transparent_mode = false,
+  transparent_mode = true,
 })
-
-function transparent_background(color)
-    color = color or "gruvbox"
-    vim.cmd.colorscheme(color)
-
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none"} )
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"} )
-end
-
-transparent_background()
