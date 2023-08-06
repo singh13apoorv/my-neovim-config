c5a570b76011d440b5dafa0f9ed47d8d6d5c4614
Apoorv Singh <singh13.apoorv@gmail.com>
2023-08-06 21:29:10 +0530

    package change

--- lua/apoorvsconfig/packer.lua
+++ lua/apoorvsconfig/packer.lua
@@ -59,7 +59,7 @@ return packer.startup(function(use)
     --configure lsp servers
     use('neovim/nvim-lspconfig')
     use('hrsh7th/cmp-nvim-lsp')
-    use('glepnir/lspsaga.nvim')
+    use({ 'nvimdev/lspsaga.nvim', branch = 'main' })
     use('onsails/lspkind.nvim')
 
     --git integration
