df9215d66c47ab5573ff55430e834071296d3b11
Apoorv Singh <singh13.apoorv@gmail.com>
2023-11-16 15:17:10 +0530

    adding python version configuration.

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
 require("ap_config.lazy")
 require("ap_config.settings.personaltweaks")
 require("ap_config.settings.remaps")
-require("ap_config.utils.python-setup")
--- lua/ap_config/utils/python-setup.lua
+++
@@ -1,7 +0,0 @@
-if vim.fn.executable('python3') == 1 then
-    vim.g.python3_host_prog = 'python3'
-elseif vim.fn.executable('python') == 1 then
-    vim.g.python3_host_prog = 'python'
-else
-    print("Warning: Python executable not found!")
-end
