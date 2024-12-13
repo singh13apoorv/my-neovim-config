829e64e64370fa47c1ca59ff3fde1c55194c6e5f
apoorvsingh13 <apoorv.singh@pepsales.ai>
2024-12-14 02:02:28 +0530

    changed tag from 0.1.x to 0.1.8

--- lua/apoorvConf/plugins/telescope.lua
+++ lua/apoorvConf/plugins/telescope.lua
@@ -19,7 +19,7 @@ end
 
 return {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.x",
+    tag = "0.1.8",
     -- or                              , branch = '0.1.x',
     dependencies = {
         "nvim-lua/plenary.nvim",
