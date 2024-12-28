4aea13a6478bff70b0773eb91bcdfd4adf84653b
apoorvsingh13 <apoorv.singh@pepsales.ai>
2024-12-29 04:45:54 +0530

    Removed tsserver from ensured install.

--- lua/apoorvConf/plugins/lsp/mason.lua
+++ lua/apoorvConf/plugins/lsp/mason.lua
@@ -26,7 +26,6 @@ return {
         mason_lspconfig.setup {
             -- list of servers for mason to install
             ensure_installed = {
-                "tsserver",
                 "html",
                 "cssls",
                 "tailwindcss",
